14561e4d1f8f6b6963209adca95b28343cf54cf7
phillipweinstock <phillipweinstock@users.noreply.github.com>
2020-05-16 21:48:52 +1000

    Add project files.

---
+++ mySwin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using mySwin.Models;
+
+namespace mySwin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
